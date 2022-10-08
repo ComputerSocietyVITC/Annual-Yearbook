f6a7b030b1eb1f3390c067aa27bea8dcb33c5f40
cool-pants <muban.sg@gmail.com>
2022-10-09 01:31:03 +0530

    [ui]: minor css changes for header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import { VscGithubAlt } from "react-icons/vsc";
 let HeaderComponent = () => {
   return (
     <>
-      <nav className="mt-4 h-auto mx-2 rounded-md bg-[#31ce9f]">
+      <nav className="h-auto rounded-b-md bg-[#31ce9f]">
         <section className="flex flex-auto justify-between items-center">
           <section className="mx-2 p-2 text-white font-black text-3xl">
             Annual Yearbook
