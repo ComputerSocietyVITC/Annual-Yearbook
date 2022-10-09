ec166402c33bd1f5f54f5d6f5460028ef5b5ae56
Abhijith Ganesh <githubwatchabhijithganesh@gmail.com>
2022-10-09 18:33:05 +0530

    Update src/components/contributors.tsx

--- src/components/contributors.tsx
+++ src/components/contributors.tsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ContributorType from "../types/ContributorType";
 import axios from "axios";
 
