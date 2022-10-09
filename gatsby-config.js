a0f866bfb41ee6516518b221cf2bd5c82628dbcf
Abhijith Ganesh <githubwatchabhijithganesh@gmail.com>
2022-10-09 17:01:57 +0530

    Linting

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,27 @@ module.exports = {
   siteMetadata: {
     title: `annual-yearbook`,
     description:`Student's yearbook is an open-source project which intends to dispaly the students who will be graduating and who've graduated through the years forming a yearbook.`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://memories.ieeecsvitc.com`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+  "gatsby-plugin-image", 
+  "gatsby-plugin-sitemap",
+  {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, 
+  "gatsby-plugin-sharp", 
+  "gatsby-transformer-sharp", 
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  },'gatsby-plugin-react-helment']
+  },
+  'gatsby-plugin-react-helment'
+  ]
 };
