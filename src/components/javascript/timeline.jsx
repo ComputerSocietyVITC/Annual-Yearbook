8e2322b346236e34119b611a9e47bd8ee6ead5c7
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2022-10-12 19:51:14 +0530

    updated timeline
    Signed-off-by: Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>

--- src/components/javascript/timeline.jsx
+++ src/components/javascript/timeline.jsx
@@ -5,7 +5,7 @@ function Timeline() {
   return (
     <div className="scrollable flex flex-row font-['Montserrat'] font-bold w-auto overflow-x-scroll justify-start">
       <div className="flex flex-col items-center">
-        <Link to="/2017" className="text-2xl font-bold hover:underline">
+        <Link to="/yearbook/2017" className="text-2xl font-bold hover:underline">
           2017
         </Link>
         <div className="h-4 w-4 relative mb-[-10px] bg-white rounded-full border-[6px] z-20 border-[#31ce9f]"></div>
@@ -16,12 +16,12 @@ function Timeline() {
         <div className="h-2 bg-[#31ce9f] w-[128px]"></div>
         <div className="h-8 w-2 relative bg-[#31ce9f] -z-10 rounded-top-full"></div>
         <div className="h-4 w-4 relative mt-[-10px] bg-white rounded-full border-[6px] z-20 border-[#31ce9f]"></div>
-        <Link to="/2018" className="text-2xl font-bold hover:underline">
+        <Link to="/yearbook/2018" className="text-2xl font-bold hover:underline">
           2018
         </Link>
       </div>
       <div className="flex flex-col items-center">
-        <Link to="/2019" className="text-2xl font-bold hover:underline">
+        <Link to="/yearbook/2019" className="text-2xl font-bold hover:underline">
           2019
         </Link>
         <div className="h-4 w-4 relative mb-[-10px] bg-white rounded-full border-[6px] z-20 border-[#31ce9f]"></div>
@@ -32,12 +32,12 @@ function Timeline() {
         <div className="h-2 bg-[#31ce9f] w-[128px]"></div>
         <div className="h-8 w-2 relative bg-[#31ce9f] -z-10 rounded-top-full"></div>
         <div className="h-4 w-4 relative mt-[-10px] bg-white rounded-full border-[6px] z-20 border-[#31ce9f]"></div>
-        <Link to="/2020" className="text-2xl font-bold hover:underline">
+        <Link to="/yearbook/2020" className="text-2xl font-bold hover:underline">
           2020
         </Link>
       </div>
       <div className="flex flex-col items-center">
-        <Link to="/2021" className="text-2xl font-bold hover:underline">
+        <Link to="/yearbook/2021" className="text-2xl font-bold hover:underline">
           2021
         </Link>
         <div className="h-4 w-4 relative mb-[-10px] bg-white rounded-full border-[6px] z-20 border-[#31ce9f]"></div>
@@ -48,7 +48,7 @@ function Timeline() {
         <div className="h-2 bg-[#31ce9f] w-[128px]"></div>
         <div className="h-8 w-2 relative bg-[#31ce9f] -z-10 rounded-top-full"></div>
         <div className="h-4 w-4 relative mt-[-10px] bg-white rounded-full border-[6px] z-20 border-[#31ce9f]"></div>
-        <Link to="/2022" className="text-2xl font-bold hover:underline">
+        <Link to="/yearbook/2022" className="text-2xl font-bold hover:underline">
           2022
         </Link>
       </div>
