67a0a01fe228e3fea8d765221827fb0803506c0b
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2022-10-16 18:16:22 +0530

    styledscrollbar
    Signed-off-by: Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>

--- src/components/javascript/timeline.jsx
+++ src/components/javascript/timeline.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "gatsby";
+import "../../styles/scrollbar.css";
 
 function Timeline() {
   return (
-    <div className="scrollable flex flex-row font-['Montserrat'] font-bold w-auto overflow-x-scroll justify-start">
+    <div className="scrollable flex flex-row font-['Montserrat'] font-bold w-auto overflow-x-scroll justify-start p-8">
       <div className="flex flex-col items-center">
         <Link to="/yearbook/2017" className="text-2xl font-bold hover:underline">
           2017
@@ -45,7 +46,7 @@ function Timeline() {
         <div className="h-2 bg-[#31ce9f] w-[128px]"></div>
       </div>
       <div className="flex flex-col justify-center items-center mt-[70px]">
-        <div className="h-2 bg-[#31ce9f] w-[128px]"></div>
+        <div className="h-2 bg-[#31ce9f] w-[128px] rounded-r-full"></div>
         <div className="h-8 w-2 relative bg-[#31ce9f] -z-10 rounded-top-full"></div>
         <div className="h-4 w-4 relative mt-[-10px] bg-white rounded-full border-[6px] z-20 border-[#31ce9f]"></div>
         <Link to="/yearbook/2022" className="text-2xl font-bold hover:underline">
