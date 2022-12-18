5fd553fec46563ead4b1863f0dcfaa64af890f96
Abhijith Ganesh <AbhijithGanesh@users.noreply.github.com>
2022-12-18 13:53:52 +0000

    deployed via Gatsby Publish Action 🎩 for 719ce41644be768f90f36af1ac58728528613fd5

---
+++ component---src-pages-years-year-tsx-a2b4880e8a5829ba34c5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkannual_yearbook=self.webpackChunkannual_yearbook||[]).push([[771],{1878:function(e,n,a){a.r(n),a.d(n,{Head:function(){return l}});var t=a(7294);n.default=function(e){var n=e.params;return(null==n?void 0:n.age)>9999?t.createElement("h1",null,"Woah, Alien, who are you!👽"):t.createElement("h1",{className:"font-bold text-3xl text-red-600"},null==n?void 0:n.year)};var l=function(){return t.createElement("title",null,"404 Page not found!")}}}]);
+//# sourceMappingURL=component---src-pages-years-year-tsx-a2b4880e8a5829ba34c5.js.map
